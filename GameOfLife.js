7cfe0ab4a3f46c8ec08b51202d8755a1e659f12b
kemvlem <asitimbaykemlem@gmail.com>
2020-07-26 20:44:37 -0400

    console board added

--- GameOfLife.js
+++ GameOfLife.js
@@ -4,35 +4,45 @@ class GameOfLife {
     this.height = height;
     this.board = this.makeBoard();
   }
-
   /**
    * Returns a 2D Array
    */
+  makeBoard(width,height) {
+    var board = [];
+    for (var i = 0; i < this.height; i++) {
+      var row = [];
 
-  makeBoard() {
-    // TODO: Create and return an 2D Array
-    // with `this.heigh` as rows and `this.width` as cols.
-    // For example, given a height of 4 and a width of 3, it will generate:
-    // [
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    //  [0, 0, 0],
-    // ]
+      for (var j = 0; j < this.width; j++) {
+        var col = 0;
+        row.push(col);
+      }
+      board.push(row)
+    }
+
+    //  var board =[];
+    return board;
   }
-
+  indexFor(row, col) {
+    //return undefined if we're out of bounds
+    if (row < 0 || row >= this.height || col < 0 || col >= this.width) {
+      return;
+    }
+    return row * this.width + col;
+  }
   /**
    * Return the amount of living neighbors around a given coordinate.
    */
-
   livingNeighbors(row, col) {
     // TODO: Return the count of living neighbors.
-  }
+    for (let i = 0; i < col; i++) {
+      for (let j = 0; j < row; j++) {
+      }
 
+    }
+  }
   /**
    * Given the present board, apply the rules to generate a new board
    */
-
   tick() {
     const newBoard = this.makeBoard();
     // TODO: Here is where you want to loop through all the cells
@@ -45,5 +55,18 @@ class GameOfLife {
     // 2. Set the next state of all cells in newBoard,
     // based on their current alive neighbors
     this.board = newBoard;
+  }
+  getCell(row, col) {
+    // create cellExist function and then do if this.cellExist() { code rest in here}
+    if (value = 1) {
+      return value = 1;
+    } else if (value = 0) {
+      return value = 0;
+    }
+
+    return value;
+
+  }
+  setCell(value, row, col) {
   }
 }
